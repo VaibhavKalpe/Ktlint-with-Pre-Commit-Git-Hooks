268dc63fb288ae59076b7ac2b200903bd7e14852
Vaibhav <vaibhav@email.com>
2020-07-10 15:02:07 +0530

    Fix styles suggested by ktlint

--- app/src/test/java/com/vaibhavkalpe/ktlintwithpre_commitgithooks/ExampleUnitTest.kt
+++ app/src/test/java/com/vaibhavkalpe/ktlintwithpre_commitgithooks/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.vaibhavkalpe.ktlintwithpre_commitgithooks
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
